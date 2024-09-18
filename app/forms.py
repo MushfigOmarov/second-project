0bc75a815267b1457afef5fa084787b048fe7152
MushfigOmarov <misha@gmail.com>
2024-09-18 19:16:42 +0200

    i deleted commits in forms.py file

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,3 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-    # '''
-    # Generate a form based on the User model with the fields:
-    # username, email, password1, password2
-    # '''
